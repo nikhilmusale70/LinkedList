b4c863c1cc6d638bc59c6ed88a87ecc3c1a7b69f
Nikhil Musale <nikhilmusale70@gmail.com>
2021-08-25 03:19:46 +0530

    [Nikhil] - added sorting method

--- src/RunnerClass.java
+++ src/RunnerClass.java
@@ -8,28 +8,31 @@ public class RunnerClass {
         MyLinkedList linkedList = new MyLinkedList();
         linkedList.add(56);
         linkedList.add(30);
-        linkedList.add(70);
+        linkedList.add(35);
         linkedList.add(22);
+        linkedList.add(11);
+        linkedList.add(88);
 
         linkedList.show();
-        System.out.println();
-        System.out.println(linkedList.totalSize());
-        System.out.println();
+
+        System.out.println("\nSorted List\n");
 
         int totalSize = linkedList.totalSize();
 
-        Node n = linkedList.head;
+        Node n;
         int tempData;
+        Node n1 = linkedList.head;
 
-        for (int i=0; i<=totalSize; i++){
-            for (int j=0; j<=totalSize; j++){
+        for (int i=0; i<totalSize-1; i++){
+            n = linkedList.head;
+            for (int j=0; j<totalSize-1; j++){
 
                 if (n.key > n.next.key){
                     tempData = n.key;
-                    n.key = n.next.key;
+                    n.key=n.next.key;
                     n.next.key = tempData;
                 }
-                n=n.next;
+                n= n.next;
             }
         }
 
